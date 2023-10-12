6459dc379165ce4229d97b29febb1d1677d7226e
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-10-11 22:36:15 -0300

    Implement unitary tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { TodoCardComponent } from './components/todo-card/todo-card.component';
@@ -11,5 +11,12 @@ import { TodoCardComponent } from './components/todo-card/todo-card.component';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  @Input() public projectName!: string;
+  @Output() public outputEvent = new EventEmitter<string>();
+
   title = 'todo-list-16';
+
+  public handleEmitEvent(): void {
+    this.outputEvent.emit(this.projectName);
+  }
 }
